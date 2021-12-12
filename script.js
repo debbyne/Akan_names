87adf9badbb3710aa71043e2cf385ac2c1a55758
debbyne <kinyadebbyne@gmail.com>
2021-12-12 18:46:26 +0300

    add footer

---
+++ script.js
@@ -0,0 +1,5 @@
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+
+
