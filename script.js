15f1703ef0a4b493e21e592a420fc6ac1b68b60d
debbyne <kinyadebbyne@gmail.com>
2021-12-12 19:04:10 +0300

    add validation of gender

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-
-
+function validate() {
+  let genders = document.getElementsByName("gender");
+else if(genders[female].checked==false && genders[male].checked==false ) {
+    alert("You must select male or female");
+    return false;
+} 
+}  
